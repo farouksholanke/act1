c376e18f31c136e42fb2a24357f9ae8483c09335
Farouk Sholanke <zayd.farouk@gmail.com>
2025-04-07 08:58:55 -0400

    script update

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt get install cowsay -y 
+sudo apt-get install cowsay -y 
 cowsay -f dragon "Run for cover, I am a Dragon!" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
